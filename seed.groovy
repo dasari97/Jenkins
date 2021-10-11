4e7f78874f52793a103df9bf8807f769915d3388
Dasari Sai Krishna <89766945+dasari97@users.noreply.github.com>
2021-10-11 17:35:34 +0530

    creating seed.groovy

---
+++ seed.groovy
@@ -0,0 +1,33 @@
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+
+def component = ["frontend", "catalogue", "cart", "shipping", "payment", "user"]
+
+def count = (component.size() -1)
+
+for(int i in 0..count) {
+
+def j=component[i]
+pipelineJob("CI-Pipelines/${j}") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/${j}")
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
+}
